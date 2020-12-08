ba247c9bfc8ee304ac81a7404330da1e101fd185
roperrott <69644462+roperrott@users.noreply.github.com>
2020-12-08 20:54:13 -0300

    mock para el template de las cervezas, foreach de las cervezas y aplicacion de clases con if

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Beer } from './Beer';
 
 @Component({
   selector: 'app-beer-list',
@@ -6,13 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beer-list.component.scss']
 })
 export class BeerListComponent implements OnInit {
-  beer = {
-    "name": "PURO HUMO",
-    "type": "Cream Stout",
-    "description":"De un profundo color oscuro con aromas tostado y café. Presenta un sabor intenso y un cuerpo y espuma cremosa. IBU 18 ALC% 4.5",
-    "price": 180,
-    "image": "assets/img/cream.png"
-  }
+  beers: Beer[] = [
+    {
+      name: 'PURO HUMO',
+      type: 'Cream Stout',
+      description:'De un profundo color oscuro con aromas tostado y café. Presenta un sabor intenso y un cuerpo y espuma cremosa. IBU 18 ALC% 4.5',
+      price: 210,
+      stock: 20,
+      image: 'assets/img/cream.png',
+      clearance: false,
+    },
+    {
+      name: 'AMBER LAGER',
+      type: 'Lager',
+      description:'Sus maltas le otorgan un color ámbar rojizo y un sabor balanceado entre el dulzor y el amargor. IBU 18 ALC% 4.5',
+      price: 190,
+      stock: 30,
+      image: 'assets/img/cream.png',
+      clearance: true,
+    },
+    {
+      name: 'PINK APA',
+      type: 'APA',
+      description:'De bajo contenido alcohólico, fuerte presencia en aroma y gusto de lúpulo americano. Destacan notas cítricas y de frutas tropicales. IBU 35 ALC% 3.5',
+      price: 200,
+      stock: 0,
+      image: 'assets/img/cream.png',
+      clearance: false,
+    },
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
