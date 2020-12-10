bbece2f62225d53288bfb18e697c8815e8463c9e
roperrott <69644462+roperrott@users.noreply.github.com>
2020-12-10 17:57:49 -0300

    aplicacion de ruteo al nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
+import { BreweryBeersComponent } from './brewery-beers/brewery-beers.component';
 
 import { FormsModule } from '@angular/forms';
+import { CartComponent } from './cart/cart.component';
+import { BreweryAboutComponent } from './brewery-about/brewery-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeerListComponent
+    BeerListComponent,
+    CartComponent,
+    BreweryBeersComponent,
+    BreweryAboutComponent
   ],
   imports: [
     BrowserModule,
